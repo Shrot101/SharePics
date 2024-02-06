bbfcc99376c1066b36b7539b6b00068938ecaa6f
samir suroshe <sameersuroshe50@gmail.com>
2024-02-06 21:36:16 +0530

    add profile pic functionality

--- loginRegisterPage/js/main.js
+++ loginRegisterPage/js/main.js
@@ -2,5 +2,19 @@
 
 	"use strict";
 
-
 })(jQuery);
+
+
+
+	const imageDisplay = document.getElementById('profilepic');
+	const imageData = document.getElementById('uploadimage');
+	const imageUrl = URL.createObjectURL(imageData.files[0]);
+	
+	imageData.onchange = function loadImageFromLocalStorage() {
+	if (imageData) {
+		// Set the image source
+		imageDisplay.style.backgroundImage =`url( ${imageUrl})`;
+	} else {
+		alert('No image found in local storage.');
+	}
+}
